ba052060efde7827e37d83c5b0f5411702c769dd
Muhammad Haikal Islami <143589943+haelahpx@users.noreply.github.com>
2025-10-31 12:19:49 +0700

    Update sidebar.blade.php

--- resources/views/livewire/components/partials/receptionist/sidebar.blade.php
+++ resources/views/livewire/components/partials/receptionist/sidebar.blade.php
@@ -7,16 +7,18 @@
         border-r border-zinc-200 dark:border-zinc-700
         lg:w-64 w-[85vw] max-w-sm
         z-40
-    ">
-
+        text-white
+    "
+>
     <flux:sidebar.header>
-        {{-- Brand: text putih + logo putih --}}
+        {{-- Brand: teks & logo dibuat putih --}}
         <flux:sidebar.brand
             href="#"
             logo="{{ asset('images/logo/kebun-raya-bogor.png') }}"
             name="Kebun Raya Bogor."
             class="text-white"
-            style="filter: brightness(0) invert(1);" />
+            style="filter: brightness(0) invert(1);"
+        />
         <flux:sidebar.collapse class="lg:hidden" />
     </flux:sidebar.header>
 
@@ -27,71 +29,80 @@
         <flux:sidebar.item
             icon="home"
             href="{{ route('receptionist.dashboard') }}"
-            :current="request()->routeIs('receptionist.dashboard')">
+            :current="request()->routeIs('receptionist.dashboard')"
+        >
             Home
         </flux:sidebar.item>
+
         <flux:sidebar.group expandable heading="Room Management" class="grid">
             <flux:sidebar.item
                 icon="calendar-days"
                 href="{{ route('receptionist.schedule') }}"
-                :current="request()->routeIs('receptionist.schedule')">
+                :current="request()->routeIs('receptionist.schedule')"
+            >
                 Booking/Meeting Room
             </flux:sidebar.item>
 
             <flux:sidebar.item
                 icon="check-circle"
                 href="{{ route('receptionist.bookings') }}"
-                :current="request()->routeIs('receptionist.bookings')">
+                :current="request()->routeIs('receptionist.bookings')"
+            >
                 Booking/Meeting Approval
             </flux:sidebar.item>
 
             <flux:sidebar.item
                 icon="clock"
                 href="{{ route('receptionist.bookinghistory') }}"
-                :current="request()->routeIs('receptionist.bookinghistory')">
+                :current="request()->routeIs('receptionist.bookinghistory')"
+            >
                 Booking/Meeting History
             </flux:sidebar.item>
         </flux:sidebar.group>
 
-
-        <flxux:sidebar.group expandable heading="Guest Management" class="grid">
+        <flux:sidebar.group expandable heading="Guest Management" class="grid">
             <flux:sidebar.item
                 icon="inbox"
                 href="{{ route('receptionist.guestbook') }}"
-                :current="request()->routeIs('receptionist.guestbook*')">
+                :current="request()->routeIs('receptionist.guestbook*')"
+            >
                 GuestBook
             </flux:sidebar.item>
 
             <flux:sidebar.item
                 icon="clock"
                 href="{{ route('receptionist.guestbookhistory') }}"
-                :current="request()->routeIs('receptionist.guestbookhistory*')">
+                :current="request()->routeIs('receptionist.guestbookhistory*')"
+            >
                 GuestBook History
             </flux:sidebar.item>
-            </flux:sidebar.group>
+        </flux:sidebar.group>
 
-            <flux:sidebar.group expandable heading="Document/Package Management" class="grid">
-                <flux:sidebar.item
-                    icon="gift"
-                    href="{{ route('receptionist.docpackform') }}"
-                    :current="request()->routeIs('receptionist.docpackform')">
-                    Document/Package Form
-                </flux:sidebar.item>
+        <flux:sidebar.group expandable heading="Document/Package Management" class="grid">
+            <flux:sidebar.item
+                icon="gift"
+                href="{{ route('receptionist.docpackform') }}"
+                :current="request()->routeIs('receptionist.docpackform')"
+            >
+                Document/Package Form
+            </flux:sidebar.item>
 
-                <flux:sidebar.item
-                    icon="clock"
-                    href="{{ route('receptionist.docpackstatus') }}"
-                    :current="request()->routeIs('receptionist.docpackstatus')">
-                    Document/Package Status
-                </flux:sidebar.item>
+            <flux:sidebar.item
+                icon="clock"
+                href="{{ route('receptionist.docpackstatus') }}"
+                :current="request()->routeIs('receptionist.docpackstatus')"
+            >
+                Document/Package Status
+            </flux:sidebar.item>
 
-                <flux:sidebar.item
-                    icon="clock"
-                    href="{{ route('receptionist.docpackhistory') }}"
-                    :current="request()->routeIs('receptionist.docpackhistory')">
-                    Document/Package History
-                </flux:sidebar.item>
-            </flux:sidebar.group>
+            <flux:sidebar.item
+                icon="clock"
+                href="{{ route('receptionist.docpackhistory') }}"
+                :current="request()->routeIs('receptionist.docpackhistory')"
+            >
+                Document/Package History
+            </flux:sidebar.item>
+        </flux:sidebar.group>
     </flux:sidebar.nav>
 
     <flux:sidebar.spacer />
@@ -101,13 +112,14 @@
         <flux:sidebar.item icon="cog-6-tooth" href="#">Settings</flux:sidebar.item>
         <flux:sidebar.item icon="information-circle" href="#">Help</flux:sidebar.item>
 
-        {{-- Logout for MOBILE --}}
+        {{-- Logout untuk MOBILE --}}
         <form method="POST" action="{{ route('logout') }}" class="lg:hidden">
             @csrf
             <flux:sidebar.item
                 icon="arrow-right-start-on-rectangle"
                 as="button"
-                type="submit">
+                type="submit"
+            >
                 Logout
             </flux:sidebar.item>
         </form>
@@ -115,29 +127,34 @@
 
     {{-- DESKTOP DROPDOWN --}}
     <flux:dropdown position="top" align="start" class="max-lg:hidden">
-        <flux:sidebar.profile avatar="" name="{{ $fullName }}" />
+        <flux:sidebar.profile
+            avatar=""
+            name="{{ $fullName ?? 'User' }}"
+        />
 
         <flux:menu>
             <flux:menu.radio.group>
-                <flux:menu.radio checked>{{ $fullName }}</flux:menu.radio>
+                <flux:menu.radio checked>{{ $fullName ?? 'User' }}</flux:menu.radio>
 
                 <flux:sidebar.item
                     icon="user"
                     href="{{ route('user.home') }}"
-                    class="cursor-pointer">
+                    class="cursor-pointer"
+                >
                     User Page
                 </flux:sidebar.item>
             </flux:menu.radio.group>
 
             <flux:menu.separator />
 
-            {{-- Logout for DESKTOP --}}
+            {{-- Logout untuk DESKTOP --}}
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <flux:menu.item
                     icon="arrow-right-start-on-rectangle"
                     as="button"
-                    type="submit">
+                    type="submit"
+                >
                     Logout
                 </flux:menu.item>
             </form>
